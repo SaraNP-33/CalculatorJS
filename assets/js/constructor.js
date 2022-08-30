24094b88e8c3c7603c262a41d76bc9f228f49d5c
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-08-30 20:53:05 +0000

    created file to write calculator constructor, started constructor and got all the datasets from index.html onto script.js

---
+++ assets/js/constructor.js
@@ -0,0 +1,29 @@
+class Calculator{
+    constructor(previousOutputTextEl, currentOutputTextEl) {
+     this.previousOutputTextEl= previousOutputTextEl
+     this.currentOutputTextEl= currentOutputTextEl
+     this.clear()
+    }
+ 
+    clear(){
+     this.currentOutputTextEl=''
+     this.previousOutputTextEl=''
+     this.operation=undefined
+    }
+    delete(){
+ 
+    }
+    appendNumber(number){
+ 
+    }
+    chooseOperation(operation){
+ 
+    }
+    compute(){
+ 
+    }
+    updateDisplay(){
+ 
+    }
+ }
+ 
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+const numBtn = document.querySelectorAll("[data-number]")
+const operationBtn= document.querySelectorAll('[data-operation]')
+const delBtn= document.querySelector('[data-del]')
+const allClear= document.querySelector('[data-all-clear]')
+const equalBtn= document.querySelector('[data-equals]')
+const previousOutputTextEl= document.querySelector('[data-previous]')
+const currentOutputTextEl=document.querySelector('[data-current]')
